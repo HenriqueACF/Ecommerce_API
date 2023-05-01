6b545c26cdc3a14051312962b1f906afff1f7450
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-05-01 17:46:12 -0300

    feat: created methods

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -1,3 +1,17 @@
 module.exports = {
+    getCategories: async (req, res)=>{
 
+    },
+    addAction: async (req, res)=>{
+
+    },
+    getList: async (req, res)=>{
+
+    },
+    addItem: async (req, res)=>{
+
+    },
+    editAction: async (req, res)=>{
+
+    }
 }
