a76960a4b95bbcc77799efc4c70a6b6e74b7eb29
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-05-02 16:18:48 -0300

    feat: init signup

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,8 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const {validationResult, matchedData} = require('express-validator')
+const User = require('../models/User')
+const State = require('../models/State')
 
 module.exports = {
     signin: async(req,res)=>{
@@ -11,6 +15,50 @@ module.exports = {
             return
         }
         const data = matchedData(req)
+
+        // verifica existencia de email
+        const user = await User.findOne({
+            email: data.email,
+        })
+        if(user){
+            res.json({
+                error:{email:{msg: 'E-mail já está sendo utilizado '}}
+            })
+            return
+        }
+
+        // verifica se o estado existe
+        if(mongoose.Types.ObjectId.isValid(data.state)){
+            const stateItem = await State.findById(data.state)
+            if(!stateItem){
+                res.json({
+                    error:{state:{msg: 'Estado não existe'}}
+                })
+                return
+            }
+        } else{
+            res.json({
+                error:{state:{msg: 'Codigo do estado invalido'}}
+            })
+            return
+        }
+
+        // criar hash da senha e token
+        const passwordHash = await bcrypt.hash(data.password, 10)
+        const payload = (Date.now()+Math.random()).toString()
+        const token = await bcrypt.hash(payload,10)
+
+        // cria usuario
+        const newUser = new User({
+            name: data.name,
+            email: data.email,
+            passwordHash,
+            token,
+            state: data.state
+        })
+        await newUser.save()
+
+        res.json({token})
 
         res.json({tudocerto:true, data})
     }
